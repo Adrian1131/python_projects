e61843ee39bf5443e9f6eeaad131375a6fdfbc2a
Adrian1131 <70630004+Adrian1131@users.noreply.github.com>
2022-02-07 22:04:06 -0500

    Add files via upload
    This is just the beginning of something great, mark my words. This is a massive work in progress, just be ready for what is gonna be included. Thank you for checking this out.

---
+++ guessthenumber.py
@@ -0,0 +1,17 @@
+import random
+
+def guess(x):
+    random_number = random.randint (1,x)
+    guess = 0
+    while guess != random_number: 
+        guess = int(input(f'Guess the number between 1 and {x}: '))
+        if guess < random_number: 
+            print ('Sorry guess again, number way to low bro.')
+
+        elif guess > random_number: 
+            print ('Sorry, guess again. Too high my man.')
+
+    print (f'Yay congrats, you guessed the number {random_number} correctly')
+            
+            
+    guess (10)
